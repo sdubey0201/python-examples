d1807d81cca14711609dac1b8d65997caa70fa17
sunil dubey <sdubey0201@gmail.com>
2024-02-25 00:23:23 +0530

    added functions for reverse string using recursion
    added functions for reverse string using recursion

--- string_reverse_recursion.py
+++ string_reverse_recursion.py
@@ -1,4 +1,4 @@
-print('hello world')
+print('Reverse string using Recursion')
 def reverse(input):
     if len(input)==0:
         return
@@ -7,6 +7,23 @@ def reverse(input):
     print(temp,end='')
 
 input_value= 'good communication skill'
+
+print('Approach 1')
 reverse(input_value)
+
+def reverse_aproach_2(input,index,size):
+    if index == size:
+        return
+    temp = input[index]
+    reverse_aproach_2(input,index+1,size)
+    print(temp,end='')
+
+size = len(input_value)
+print('Approach 2')
+reverse_aproach_2(input_value,0,size)
+
+
+
+
 
 
